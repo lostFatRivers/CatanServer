32abaadead70661794ae8406a38d5a5e9defb0df
Joker <495669631@qq.com>
2020-08-23 22:59:52 +0800

    升级vertx

---
+++ Catan/src/test/java/com/jokerbee/test/request/HttpTest.java
@@ -0,0 +1,12 @@
+package com.jokerbee.test.request;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpTest {
+    protected static Logger logger = LoggerFactory.getLogger("HttpTest");
+
+    public static void main(String[] args) {
+        logger.info("Good Day");
+    }
+}
